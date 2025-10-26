705c5f1c673d96d3230505aace6d620762a30fe9
Cauê Urbini <caueurbini@gmail.com>
2025-10-26 14:11:31 -0300

    feat: initial blog architecture

---
+++ BLOG/templates/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>Caue &copy; 2025</p>
+</footer>
+</body>
+</html>
+
---
+++ BLOG/templates/header.php
@@ -0,0 +1,24 @@
+<?php 
+
+include_once("templates/headers.php");
+?>
+<?php 
+
+    include_once("helpers/url.php")
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <link rel="stylesheet" href="<?=$BASE_URL ?>/css/styles.css">
+
+</head>
+<body>
+    <h1> Olá Mundo! </h1>
+
+</body>
+</html>
